4a0645947ad708c0c6fffcf517fb932c1e032094
YALLICO OWNER <yallico2024@gmail.com>
2026-08-15 23:47:23 -0500

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -2,20 +2,20 @@ let handler = async (m, { conn }) => {
     try {
         await m.react('🔗')
         let link = await conn.groupInviteCode(m.chat)
-        
-        let texto = `🐉 𓆩 𝗟𝗜𝗡𝗞 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 𓆪 🐉
 
-.⃟𖥔 ݁. 𖦹˙— \`\`INVITACION\`\` —˙𖦹.🏆꒷
+        let texto = `🐸 𓆩 𝗟𝗜𝗡𝗞 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 𓆪 🐸
 
-──愛 *𝗟𝗜𝗡𝗞* ╏ ⚡
+.⃟𖥔 ݁. 𖦹˙— \`\`INVITACION\`\` —˙𖦹.🍃꒷
+
+──🍃 *𝗟𝗜𝗡𝗞* ╏ 💚
 https://chat.whatsapp.com/${link}
 
-──愛 *𝗡𝗢𝗧𝗔* ╏ ⚡
-⚡ *Solo admins pueden resetear el link*
-⚡ *No lo compartas con desconocidos*
+──🍃 *𝗡𝗢𝗧𝗔* ╏ 🌿
+🌿 *Solo admins pueden resetear el link*
+🌿 *No lo compartas con desconocidos*
 
 ━━━━━━━━━━━
-*Powered by*: SON GOKU PREM 💥`
+*Powered by*: SAPITO BOT PREM 🍃`
 
         await conn.reply(m.chat, texto, m)
     } catch (e) {
