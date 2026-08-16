851196c8549f13ab6da97e14962537368a3be7cc
YALLICO OWNER <yallico2024@gmail.com>
2026-08-15 23:42:08 -0500

    Actualizar group-mute.js

--- plugins/group-mute.js
+++ plugins/group-mute.js
@@ -2,7 +2,7 @@ let mutedUsers = new Set()
 
 let handler = async (m, { conn, command, participants }) => {
     let mentionedJid = m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : false
-    if (!mentionedJid) return m.reply(`🐉 𓆩 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 𓆪 🐉
+    if (!mentionedJid) return m.reply(`🐸 𓆩 𝗦𝗔𝗣𝗜𝗧𝗢 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 𓆪 🐸
 
 *Uso:*
 .mute @user → Para mutear
@@ -11,29 +11,29 @@ let handler = async (m, { conn, command, participants }) => {
 > *Etiqueta a una persona o responde a un mensaje*`)
 
     let isUserAdmin = participants.find(p => p.id === mentionedJid)?.admin
-    if (isUserAdmin) return m.reply(`⚠️ *No puedes mutear a un administrador.*`)
-    if (mentionedJid === conn.user.jid) return m.reply(`⚠️ *No puedo mutearme a mi mismo.*`)
+    if (isUserAdmin) return m.reply(`🌿 *No puedes mutear a un administrador.*`)
+    if (mentionedJid === conn.user.jid) return m.reply(`🌿 *No puedo mutearme a mi mismo.*`)
 
     if (command === "mute") {
         if (mutedUsers.has(mentionedJid)) return m.reply(`📛 *Este usuario ya está muteado*`)
         mutedUsers.add(mentionedJid)
         await m.react('🔇')
-        conn.reply(m.chat, `🐉 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐉
+        conn.reply(m.chat, `🐸 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐸
 
 🔇 *Usuario:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
-> *Sus mensajes serán eliminados automaticamente*`, m, { mentions: [mentionedJid, m.sender] })
+> *Sus mensajes serán eliminados automaticamente* 🍃`, m, { mentions: [mentionedJid, m.sender] })
     } else if (command === "unmute") {
-        if (!mutedUsers.has(mentionedJid)) return m.reply(`✅ *Este usuario no está muteado*`)
+        if (!mutedUsers.has(mentionedJid)) return m.reply(`💚 *Este usuario no está muteado*`)
         mutedUsers.delete(mentionedJid)
         await m.react('🔊')
-        conn.reply(m.chat, `🐉 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗦𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐉
+        conn.reply(m.chat, `🐸 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗦𝗠𝗨𝗧𝗘𝗔𝗗𝗢 𓆪 🐸
 
 🔊 *Usuario:* @${mentionedJid.split('@')[0]}
 👑 *Por:* @${m.sender.split('@')[0]}
 
-> *Ya puede volver a escribir*`, m, { mentions: [mentionedJid, m.sender] })
+> *Ya puede volver a croar* 🍃`, m, { mentions: [mentionedJid, m.sender] })
     }
 }
 
