e4fff81c16783c0aa9b61e3e81ec609ea54f12cd
YALLICO OWNER <yallico2024@gmail.com>
2026-08-19 10:54:55 -0500

    Actualizar group-banchat-unbanchat.js

--- plugins/group-banchat-unbanchat.js
+++ plugins/group-banchat-unbanchat.js
@@ -1,43 +1,43 @@
-let handler = async (m, { conn, isOwner, isAdmin, isROwner, command }) => {
+let handler = async (m, { conn, isOwner, isROwner, command }) => {
   if (!m.isGroup) return
   let chat = global.db.data.chats[m.chat]
   let type = command.toLowerCase()
 
-  if (!(isAdmin || isOwner || isROwner)) {
-    return conn.reply(m.chat, `🐸 𓆩 𝗦𝗔𝗣𝗜𝗧𝗢 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 𓆪 🐸\n\n🌿 *Solo admins pueden usar este comando*`, m)
+  if (!(isOwner || isROwner)) {
+    return conn.reply(m.chat, `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Solo owners pueden usar este comando*`, m)
   }
 
   switch (type) {
     case 'banchat': case 'banearchat':
-      if (chat.isBanned) return m.reply(`🐸 𓆩 𝗦𝗔𝗣𝗜𝗧𝗢 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 𓆪 🐸\n\n🌿 *Este chat ya se encuentra baneado.*`)
+      if (chat.isBanned) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Este chat ya se encuentra baneado.*`)
       chat.isBanned = true
-      await conn.reply(m.chat, `🐸 𓆩 𝗖𝗛𝗔𝗧 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐸
+      await conn.reply(m.chat, `🐱 𓆩 𝗖𝗛𝗔𝗧 𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐱
 
-.⃟𖥔 ݁. 𖦹˙— \`\`BAN\`\` —˙𖦹.🍃꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`BAN\`\` —˙𖦹.🍕꒷
 
 🚫 *El bot ha sido desactivado en este grupo*
-💬 *No responderé a ningún comando hasta que sea desbloqueado*
+💤 *No responderé a ningún comando hasta que sea desbloqueado*
 
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: SAPITO BOT PREM 🍃`, m, { mentions: [m.sender] })
+*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [m.sender] })
       break
 
     case 'unbanchat': case 'desbanearchat':
-      if (!chat.isBanned) return m.reply(`🐸 𓆩 𝗦𝗔𝗣𝗜𝗧𝗢 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 𓆪 🐸\n\n🌿 *Este chat no está baneado.*`)
+      if (!chat.isBanned) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n🍕 *Este chat no está baneado.*`)
       chat.isBanned = false
-      await conn.reply(m.chat, `🐸 𓆩 𝗖𝗛𝗔𝗧 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐸
+      await conn.reply(m.chat, `🐱 𓆩 𝗖𝗛𝗔𝗧 𝗗𝗘𝗦𝗕𝗔𝗡𝗘𝗔𝗗𝗢 𓆪 🐱
 
-.⃟𖥔 ݁. 𖦹˙— \`\`UNBAN\`\` —˙𖦹.🍃꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`UNBAN\`\` —˙𖦹.🍕꒷
 
-🌀 *El bot vuelve a estar activo en este grupo*
-💚 *Ya pueden utilizar todos los comandos con normalidad*
+😼 *El bot vuelve a estar activo en este grupo*
+🍕 *Ya pueden utilizar todos los comandos con normalidad*
 
 👑 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: SAPITO BOT PREM 🍃`, m, { mentions: [m.sender] })
+*Powered by*: ***Garfield Bot Oficial*** 🍕`, m, { mentions: [m.sender] })
       break
 
     default:
@@ -48,7 +48,7 @@ let handler = async (m, { conn, isOwner, isAdmin, isROwner, command }) => {
 handler.help = ['banchat', 'unbanchat']
 handler.tags = ['grupos']
 handler.command = /^(banchat|banearchat|unbanchat|desbanearchat)$/i
-handler.admin = true
+handler.owner = true
 handler.group = true
 
 export default handler
