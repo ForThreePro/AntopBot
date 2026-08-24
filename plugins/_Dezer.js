6fa6501749fb52d65e09f5ce415a9c200676a551
YALLICO OWNER <yallico2024@gmail.com>
2026-08-24 08:26:38 -0500

    Actualizar _Dezer.js

--- plugins/_Dezer.js
+++ plugins/_Dezer.js
@@ -1,19 +1,19 @@
 const rmbg = await import('../../storage/script/removebg.js');
 
-export default {
-  name: 'removebg',
-  tags: 'tools',
-  command: ['removebg'],
-  description: 'Quitar fondo en fotos',
-  example: '',
-  limit: true,
-  run: async(m, { sock, args }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) return m.reply('Umhhh... y la foto??')
-    m.reply(global.status.wait)
+    await m.react('⏳')
     let dor = await rmbg.removeBg(await q.download());
     let buffer = Buffer.from(dor, 'base64');
-    await sock.sendMessage(m.chat, { image: buffer, caption: 'Se eliminó con éxito el fondo de la foto.' }, { quoted: m })
-  }
+    await conn.sendMessage(m.chat, { image: buffer, caption: 'Se eliminó con éxito el fondo de la foto.' }, { quoted: m })
+    await m.react('✅')
 }
+
+handler.help = ['removebg']
+handler.tags = ['tools']
+handler.command = ['removebg']
+handler.limit = true
+
+export default handler
