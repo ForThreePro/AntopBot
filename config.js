dff53f0824cfd359a58634aa92bc3b0be865c22a
YALLICO OWNER <yallico2024@gmail.com>
2026-08-27 12:30:04 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   [ '51927174369', 'Barboza OFC 🌃', true ],
-  [ '573155227977', 'Jota 🐼', true ]
+  [ '56920592945', 'Jota 🐼', true ]
 ]
 
 global.mods = []
