f4be19a8d9d89dcda461e03cd80869b919487fad
YALLICO OWNER <yallico2024@gmail.com>
2026-08-27 11:48:25 -0500

    Actualizar busquedas-googlesearch.js

--- plugins/busquedas-googlesearch.js
+++ plugins/busquedas-googlesearch.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
     let user = `@${m.sender.split('@')[0]}`
     let groupName = m.isGroup? (await conn.groupMetadata(m.chat)).subject : 'Privado'
 
-    if (!text) return m.reply(`🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱\n\n✨ *¿Qué quieres buscar?*\n📌 *Ejemplo:* ${m.prefix}google garfield comiendo lasaña`)
+    if (!text) return m.reply(`💙 𐔌 ꒱ ***𝗔𝗡𝗧𝗜𝗧𝗢𝗣 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𐔌 ꒱ 💙\n\n✨ *¿Qué quieres buscar lindo?* 🫧\n📌 *Ejemplo:* ${m.prefix}google mariposas azules`)
 
     await m.react('🔍')
 
@@ -14,17 +14,17 @@ let handler = async (m, { conn, text }) => {
 
         if (!results.length) {
             await m.react('❌')
-            return m.reply('🍕 *No encontré resultados.*')
+            return m.reply('🫐 Ay no~ *No encontré resultados* 🌀')
         }
 
-        let txt = `🐱 𓆩 𝗕𝗨𝗦𝗖𝗔𝗗𝗢𝗥 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𓆪 🐱
+        let txt = `🫧 𐔌 ꒱ 𝗕𝗨𝗦𝗖𝗔𝗗𝗢𝗥 𝗔𝗡𝗧𝗜𝗧𝗢𝗣 𐔌 ꒱ 🫧
 
-.⃟𖥔 ݁. 𖦹˙— \`\`RESULTADOS\`\` —˙𖦹.🍕꒷
+.⃟𖥔 ݁. 𖦹˙— \`\`RESULTADOS\`\` —˙𖦹.💙꒷
 
 🔎 *Buscando:* ${text}
 
 ${results.map((v, i) => {
-            return `*${i + 1}.* *${v.title}*
+            return `*${i + 1}.* *${v.title}* 🦋
 🕒 *Duración:* ${v.timestamp}
 👁️ *Vistas:* ${v.views}
 👤 *Canal:* ${v.author.name}
@@ -35,8 +35,8 @@ ${results.map((v, i) => {
 🏷 *Grupo:* ${groupName}
 
 ━━━━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕
-*Tip:* Usa .ytmp4 o .ytmp3 + el link`
+*Powered by*: ***Antitop Bot*** 💙
+*Tip:* Usa .ytmp4 o .ytmp3 + el link 🪼`
 
         await conn.reply(m.chat, txt, m, { mentions: [m.sender] })
         await m.react('✅')
@@ -44,7 +44,7 @@ ${results.map((v, i) => {
     } catch (e) {
         console.error(e)
         await m.react('❌')
-        m.reply('🍕 *Error:* No se pudo realizar la búsqueda.')
+        m.reply('🌀 Ups~ *Error:* No se pudo realizar la búsqueda.')
     }
 }
 
