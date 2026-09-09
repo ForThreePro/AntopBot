5079317869a984f8107ad024b0d77b1afcc48a6d
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 18:54:18 -0500

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -59,8 +59,8 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
 
   if (fail) return
 
-  // IMAGEN GLOBAL ANTITOP
-  let catalogoImg = { url: global.botimg }
+  // IMAGEN FIJA ANTITOP
+  let catalogoImg = { url: 'https://files.evogb.win/1FbQzR.jpg' }
 
   let estadoTexto = isEnable? 'Activado 🫧' : 'Desactivado ✖️'
   let estadoEmoji = isEnable? '🟢' : '🔴'
