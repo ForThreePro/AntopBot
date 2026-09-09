e61263a438e77d575f40381f742b3dd238d9f3ec
YALLICO OWNER <yallico2024@gmail.com>
2026-09-09 18:56:57 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,8 +6,8 @@ let handler = async (m, { conn, usedPrefix }) => {
 
   let taguser = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
 
-  // IMAGEN GLOBAL
-  let img = { url: global.botimg }
+  // IMAGEN FIJA
+  let img = { url: 'https://files.evogb.win/1FbQzR.jpg' }
 
   let uptime = process.uptime() * 1000
   let _uptime = clockString(uptime)
